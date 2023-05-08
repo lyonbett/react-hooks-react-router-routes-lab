ea40b296634dfdb203ee7c3236dc966489caeef2
lyonbett <lyon.bett@student.moringaschool.com>
2023-05-08 12:11:07 +0300

    completed assigment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import './App.css'
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <BrowserRouter>
+    <div className="Link">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/actors">
+          <Actors />
+        </Route>
+        <Route exact path="/directors">
+          <Directors />
+        </Route>
+        <Route exact path="/movies">
+          <Movies />
+        </Route>
+      </Switch>
+    </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
